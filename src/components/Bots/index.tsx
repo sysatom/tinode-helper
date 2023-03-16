a8a6828014bf90a388b1a0d74aa5ce74ba88e86c
sysatom <sysatom@gmail.com>
2023-03-16 15:43:37 +0800

    update switch storage

--- src/components/Bots/index.tsx
+++ src/components/Bots/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import useSWR from "swr";
 import {actionFetcher} from "~/helpers/fetcher";
 import {IBot} from "~/types";
@@ -9,6 +9,23 @@ interface IBotsProps {
 }
 
 const Bots = ({selectedBot}: IBotsProps) => {
+    let [botSwitch, setBotSwitch] = useState<Map<String, boolean>>(() => {
+        const value = localStorage.getItem("bot-switch")
+        if (value) {
+            let result = JSON.parse(value)
+            if (result) {
+                let m = result as Array<any>
+                if (m != undefined) {
+                    const newMap = new Map<String, boolean>()
+                    m.forEach(([key, value]) => {
+                        newMap.set(key, value);
+                    });
+                    return newMap
+                }
+            }
+        }
+        return new Map<string, any>()
+    })
     const {data, error} = useSWR<IBot, Error>("bots", actionFetcher)
 
     useEffect(() => {
@@ -16,6 +33,13 @@ const Bots = ({selectedBot}: IBotsProps) => {
            console.log(selectedBot)
        }
     });
+
+    const handleUpdateValue = (key:string, newValue:boolean) => {
+        const newMap = new Map<String, boolean>(Array.from(botSwitch).map(([k, v]) => (k === key ? [k, newValue] : [k, v])));
+        newMap.set(key, newValue)
+        localStorage.setItem("bot-switch", JSON.stringify(Array.from(newMap)))
+        setBotSwitch(newMap);
+    };
 
     return (
         <div>{
@@ -28,6 +52,8 @@ const Bots = ({selectedBot}: IBotsProps) => {
                         <Switch.Root
                             className="w-[42px] h-[25px] bg-blackA9 rounded-full relative shadow-[0_2px_10px] shadow-blackA7 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default"
                             id="airplane-mode"
+                            defaultChecked={botSwitch.get(b.id)}
+                            onCheckedChange={(checked: boolean) => {handleUpdateValue(b.id, checked)}}
                         >
                             <Switch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA7 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
                         </Switch.Root>
